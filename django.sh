d3a92a2fec2a8ffd1e4f2be41ec0413b85683d34
Carlos Dip <cadudip@hotmail.com>
2021-10-28 10:55:21 -0300

    Fixed django script. Django base-image assembled successfully.

--- django.sh
+++ django.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+cd /home/ubuntu
 sudo apt update
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential -y
 git clone https://github.com/raulikeda/tasks.git
 cd tasks
 sed -i "s/node1/IPDB/g" ./portfolio/settings.py
 sed -i "s/django.db.backends.postgresql/django.db.backends.mysql/g" ./portfolio/settings.py
 sed -i "s/'USER': 'cloud'/'USER': 'root'/g" ./portfolio/settings.py
 sed -i "s/5432/3306/g" ./portfolio/settings.py
+echo "mysqlclient" >> requirements.txt
 ./install.sh
 sudo reboot
